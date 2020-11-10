5f417f0ebca2fea2c0810057e0e6bfd5d27a570c
jiongyi-cao <jiongyi@uchicago.edu>
2020-11-09 22:52:35 -0600

    add agent-base model

--- scripts/agent_simulation.py
+++ scripts/agent_simulation.py
@@ -22,5 +22,77 @@ to 0? Are there parameter regimes where everyone is eventually infected?
 """
 
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import randint, rand
+from sir import *
 sys.path.append("..")
-from sir import * # you can use Person() now
+
+def count_infected(pop):
+    return sum(p.is_infected() for p in pop)
+def count_recovered(pop):
+    return sum(p.is_recovered() for p in pop)
+def count_susceptible(pop,N):
+    return N - count_infected(pop) - count_recovered(pop)
+
+def run_sim(b, k, N, T):
+    """
+    return the number of people enlightened at time T
+    """
+    pop = [Person() for i in range(N)] # our population
+    infect = randint(N, size = int(0.001*N))
+    for i in infect:
+        pop[i].infect()
+    counts_I = [count_infected(pop)]
+    counts_R = [count_recovered(pop)]
+    counts_S = [count_susceptible(pop,N)]
+    for t in range(T):
+    # update the population
+        for i in range(N):
+            if pop[i].is_infected():
+        # person i infect susceptible with infection rate b/N
+                for j in range(N):
+                    if not pop[j].is_infected() and not pop[j].is_recovered():
+                        if rand() < b/N:
+                            pop[j].infect()
+        # infected population get recovered with rate k
+            if rand() < k:
+                pop[i].recover()
+          # add to our counts
+        counts_I.append(count_infected(pop))
+        counts_R.append(count_recovered(pop))
+        counts_S.append(count_susceptible(pop,N))
+    return  counts_I,counts_R,counts_S
+
+# plot how s, i, and r change over the length of the simulation
+sim1 = run_sim(5,0.01,5000,20)
+print(sim1[0])
+plt.plot(sim1[2],label = "Susceptible")
+plt.plot(sim1[0],label = "Infected")
+plt.plot(sim1[1],label = "Recovered")
+plt.legend()
+plt.show()
+
+T = 20
+N = 1000
+bs = np.arange(1,3, dtype=np.int64)
+ks = np.logspace(-2,-1,10)
+cts_I = np.zeros((len(bs), len(ks),T+1))
+cts_R = np.zeros((len(bs), len(ks),T+1))
+cts_S = np.zeros((len(bs), len(ks),T+1))
+
+for i, b in enumerate(bs):
+    for j, k in enumerate(ks):
+        cts_I[i,j,],cts_R,cts_S = run_sim(b,k,N,T)
+#phase diagram at time t
+t = 10
+plt.figure(figsize=(10,5))
+plt.imshow(cts_I[:len(bs),:len(ks),t], extent=[np.min(bs), np.max(bs),np.min(ks), np.max(ks)])
+plt.colorbar()
+# plt.axis('square')
+plt.yscale('log')
+plt.xlabel('b')
+plt.ylabel('k')
+plt.show()
+
+
--- sir/agent.py
+++ sir/agent.py
@@ -1,5 +1,45 @@
-"""
-This module implements an agent-based model.
-It defines a class which represents a person, with an internal state which is
-one of S, I or R.
-"""
+
+
+
+
+class Person():
+    """
+    This module implements an agent-based model.
+    It defines a class which represents a person, with an internal state which is
+    one of S, I or R.
+
+    By default, a person is not susceptible.  They become infected using the infect method,
+    and recovered by recover method.
+
+    """
+
+
+    def __init__(self):
+        # self.S = True # default setting not I not R -> person is susceptible
+        self.I = False  # if I = True, the person has infected
+        self.R = False  # if R = True, the person has recovered and cannot be infected
+
+    def is_infected(self):
+        """
+        returns true if the person has infected
+        """
+        return self.I
+
+    def is_recovered(self):
+        """
+        returns true if the person has recovered
+        """
+        return self.R
+
+    def infect(self):
+        """
+        person get infected
+        """
+        self.I = True
+
+    def recover(self):
+        """
+        person get recovered and cannot get infected later
+        """
+        self.R = True
+        self.I = False
