5c734483005c0211670b2225fe7d8d011fe0769a
Dennis Zheng <denniszheng@uchicago.edu>
2020-11-12 15:50:13 -0600

    write test_agent()
    test if boolean attributes S, I, R are correct

--- test/test.py
+++ test/test.py
@@ -8,16 +8,31 @@ sys.path.append("..")
 from sir import *
 
 
+
 class Test_models(unittest.TestCase):
+    
     def test_agent(self):
-        pass
+        p = Person()
+        self.assertTrue(p.S)
+        self.assertFalse(p.I)
+        self.assertFalse(p.R)
+        p.infec()
+        self.assertFalse(p.S)
+        self.assertTrue(p.I)
+        self.assertFalse(p.R)
+        p.recover()
+        self.assertFalse(p.S)
+        self.assertFalse(p.I)
+        self.assertTrue(p.R)
 
     def test_ode(self):
-        N = 1000
+        N = 100
         i0 = 0.1/100
-        t = 20
-        b = 10
-        k = 0.1
-
-        s,i,r = ode_model(N, i0, t, b, k)
-        self.assertTrue(s + i + r == 1)
+        Ts = [1,10,100]
+        bs = [1,5,10]
+        ks = [0.01, 0.1,1]
+        for T in Ts:
+            for b in bs:
+                for k in ks:
+                    s,i,r = ode_model(N, i0, T, b, k)
+                    self.assertTrue(s + i + r == 1)
