6f5c718f51b3089ff5094158e71183a899974012
Dennis Zheng <denniszheng@uchicago.edu>
2020-11-12 18:04:08 -0600

    change path and update test_ode()

--- test/test.py
+++ test/test.py
@@ -2,21 +2,23 @@
 Implement tests for the two models
 """
 
+
 import unittest
 import sys
-sys.path.append("..")
+import os
+sys.path.append(os.getcwd())
 from sir import *
 
 
 
 class Test_models(unittest.TestCase):
-    
+
     def test_agent(self):
         p = Person()
         self.assertTrue(p.S)
         self.assertFalse(p.I)
         self.assertFalse(p.R)
-        p.infec()
+        p.infect()
         self.assertFalse(p.S)
         self.assertTrue(p.I)
         self.assertFalse(p.R)
@@ -26,13 +28,15 @@ class Test_models(unittest.TestCase):
         self.assertTrue(p.R)
 
     def test_ode(self):
-        N = 100
         i0 = 0.1/100
-        Ts = [1,10,100]
+        T = 100
         bs = [1,5,10]
         ks = [0.01, 0.1,1]
-        for T in Ts:
-            for b in bs:
-                for k in ks:
-                    s,i,r = ode_model(N, i0, T, b, k)
-                    self.assertTrue(s + i + r == 1)
+        for b in bs:
+            for k in ks:
+                s,i,r = ode_model(i0, T, b, k)
+                for t in range(len(s)): # every day
+                    self.assertTrue(s[t]>=0, f'fail when t={t}, b={b}, k={k}: 0>s[t]={s[t]}')
+                    self.assertTrue(i[t]>=0, f'fail when t={t}, b={b}, k={k}: 0>i[t]={i[t]}')
+                    self.assertTrue(r[t]>=0, f'fail when t={t}, b={b}, k={k}: 0>r[t]={r[t]}')
+                    self.assertAlmostEqual(s[t]+i[t]+r[t],1, msg= f'fail when t={t}, b={b}, k={k}: s+i+r={s[t]+i[t]+r[t]}')
